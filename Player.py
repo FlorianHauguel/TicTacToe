e4b3bddefdcc937ba59bcb218a95444098658886
FlorianHauguel <florian.hauguel@gmail.com>
2020-06-09 03:24:07 +0200

    cleaning class player

--- Player.py
+++ Player.py
@@ -3,10 +3,3 @@ import enum
 class Player(enum.Enum):
    Player1 = 'x'
    Player2 = 'o'
-
-def PlayerNameFromValue(val):
-	if val == 'x':
-		return Player.Player1.name
-	if val == 'o':
-		return Player.Player2.name
-	return -1
